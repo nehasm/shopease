2eee61c6144f22a95582a8559e4be855ad71a435
neha-malvia <neha.malvia@freshworks.com>
2023-06-28 18:53:44 +0530

    revert unwanted changes

--- models/user.js
+++ models/user.js
@@ -66,7 +66,7 @@ userSchema.pre("save",async function (next){
 //jwt token
 userSchema.methods.getJWTToken = function () {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
+      expiresIn: '1d'
     });
   };
   
