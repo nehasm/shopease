c670e3851f707b43266de421d0079e78aeae44d7
neha-malvia <neha.malvia@freshworks.com>
2023-06-29 01:07:46 +0530

    setting expire chnage

--- utils/utilities.js
+++ utils/utilities.js
@@ -5,7 +5,7 @@ exports.sendToken = (user,status,res) => {
     const token = user.getJWTToken();
     const options = {
         expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+          Date.now() + 1 * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
         secure : true,
