8ad9c869a2862d003cdcaadac8df85a0c49fe48f
neha-malvia <neha.malvia@freshworks.com>
2023-06-30 22:26:39 +0530

    logout func bug fixes

--- controllers/user.js
+++ controllers/user.js
@@ -29,7 +29,7 @@ exports.loginUser = catchAsync( async (req,res,next) => {
 
 exports.logoutUser = catchAsync(async(req,res,next) => {
   const options = {
-    expires: new Date(Date.now()),
+    maxAge: -1,
     httpOnly: true,
     secure : true,
     sameSite: 'none',
